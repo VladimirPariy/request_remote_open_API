c4a780b562ba33cac1d17b51a244384517c88c23
Vladimir <87824670+VladimirPariy@users.noreply.github.com>
2022-10-19 10:04:19 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,1 +1,11 @@
-// const fs = require('fs')// const fetch = require('node-fetch')import fs from 'fs';import fetch from 'node-fetch'async function fetchData() {  let data = await fetch('https://jsonplaceholder.typicode.com/users').then(data => data.json())  const writeStream = fs.createWriteStream('./placeholder.json');  writeStream.write(JSON.stringify(data, null, 2))}fetchData()
+import fs from 'fs';
+import fetch from 'node-fetch'
+
+async function fetchData() {
+  let data = await fetch('https://jsonplaceholder.typicode.com/users').then(data => data.json())
+
+  const writeStream = fs.createWriteStream('./placeholder.json');
+  writeStream.write(JSON.stringify(data, null, 2))
+}
+
+fetchData()
